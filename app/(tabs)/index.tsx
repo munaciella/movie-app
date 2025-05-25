b23d12bf144a0d10bf570f5861101c9f20e3831e
munaciella <franes88@gmail.com>
2025-05-25 23:51:17 +0100

    create tabs/nav bar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { images } from "@/constants/images";
+import { Image, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image 
+      source={images.bg}
+      className="absolute w-full z-0" 
+      />
+    </View>
+  );
+}
